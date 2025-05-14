2a78f21fbd01905fe6da29dd01c600922d6f55f2
Andrii Dmytrenko <refresh.xss@gmail.com>
2025-05-14 20:47:23 +0100

    Simplify worker execution

--- src/lib.rs
+++ src/lib.rs
@@ -9,7 +9,7 @@ use queue::QueueConfig;
 use sqlx::{Pool, Postgres};
 use std::collections::HashMap;
 use std::sync::Arc;
-use tokio::sync::{Mutex, Semaphore, mpsc};
+use tokio::sync::{Semaphore, mpsc};
 
 pub use crate::error::OxanusError;
 pub use crate::job_envelope::JobEnvelope;
@@ -52,7 +52,10 @@ impl<DT, ET> Config<DT, ET> {
     }
 }
 
-pub async fn run<DT: Send + Sync + Clone + 'static, ET: std::error::Error + Send + Sync + 'static>(
+pub async fn run<
+    DT: Send + Sync + Clone + 'static,
+    ET: std::error::Error + Send + Sync + 'static,
+>(
     pool: &Pool<Postgres>,
     config: Config<DT, ET>,
     data: WorkerState<DT>,
@@ -76,7 +79,10 @@ pub async fn run<DT: Send + Sync + Clone + 'static, ET: std::error::Error + Send
     Ok(())
 }
 
-pub async fn setup<DT: Send + Sync + Clone + 'static, ET: std::error::Error + Send + Sync + 'static>(
+pub async fn setup<
+    DT: Send + Sync + Clone + 'static,
+    ET: std::error::Error + Send + Sync + 'static,
+>(
     pool: &Pool<Postgres>,
     config: &Config<DT, ET>,
 ) -> Result<(), OxanusError> {
@@ -89,7 +95,11 @@ pub async fn setup<DT: Send + Sync + Clone + 'static, ET: std::error::Error + Se
     Ok(())
 }
 
-pub async fn enqueue<T, DT: Send + Sync + Clone + 'static, ET: std::error::Error + Send + Sync + 'static>(
+pub async fn enqueue<
+    T,
+    DT: Send + Sync + Clone + 'static,
+    ET: std::error::Error + Send + Sync + 'static,
+>(
     pool: &Pool<Postgres>,
     queue: &Queue,
     job: T,
@@ -102,7 +112,11 @@ where
     enqueue_in(pool, queue, job, 0).await
 }
 
-pub async fn enqueue_in<T, DT: Send + Sync + Clone + 'static, ET: std::error::Error + Send + Sync + 'static>(
+pub async fn enqueue_in<
+    T,
+    DT: Send + Sync + Clone + 'static,
+    ET: std::error::Error + Send + Sync + 'static,
+>(
     pool: &Pool<Postgres>,
     queue: &Queue,
     job: T,
@@ -122,7 +136,10 @@ where
     Ok(msg_id)
 }
 
-pub async fn run_queue_workers<DT: Send + Sync + Clone + 'static, ET: std::error::Error + Send + Sync + 'static>(
+pub async fn run_queue_workers<
+    DT: Send + Sync + Clone + 'static,
+    ET: std::error::Error + Send + Sync + 'static,
+>(
     pgmq: pgmq::PGMQueue,
     config: Arc<Config<DT, ET>>,
     data: WorkerState<DT>,
@@ -137,45 +154,21 @@ pub async fn run_queue_workers<DT: Send + Sync + Clone + 'static, ET: std::error
     pgmq.create(&queue_config.name).await.ok();
 
     let concurrency = queue_config.concurrency;
-    let (job_tx, job_rx) = mpsc::channel::<WorkerEvent<DT, ET>>(concurrency);
     let (result_tx, result_rx) = mpsc::channel::<Result<(), ET>>(concurrency);
-    let job_rx = Arc::new(Mutex::new(job_rx));
     let worker_capacity = Arc::new(Semaphore::new(concurrency));
-
-    for i in 0..concurrency {
-        let job_rx = Arc::clone(&job_rx);
-        let result_tx = result_tx.clone();
-        let pgmq = pgmq.clone();
-        let queue = queue_config.name.clone();
-        let worker_capacity = Arc::clone(&worker_capacity);
-        tokio::spawn(run_worker(
-            i,
-            pgmq,
-            queue,
-            job_rx,
-            result_tx,
-            worker_capacity,
-            data.clone(),
-        ));
-    }
-
-    drop(result_tx);
 
     tokio::spawn(collect_results(result_rx));
 
     loop {
-        if worker_capacity.available_permits() == 0 {
-            tokio::time::sleep(std::time::Duration::from_millis(50)).await;
-            continue;
-        }
-
-        let msg: Option<pgmq::Message<serde_json::Value>> = match pgmq.read(&queue_config.name, Some(30)).await {
-            Ok(msg) => msg,
-            Err(e) => {
-                println!("Failed to read job from queue: {}", e);
-                continue;
-            }
-        };
+        let permit = worker_capacity.clone().acquire_owned().await.unwrap();
+        let msg: Option<pgmq::Message<serde_json::Value>> =
+            match pgmq.read(&queue_config.name, Some(30)).await {
+                Ok(msg) => msg,
+                Err(e) => {
+                    println!("Failed to read job from queue: {}", e);
+                    continue;
+                }
+            };
         if let Some(job_msg) = msg {
             let envelope: JobEnvelope = match serde_json::from_value(job_msg.message) {
                 Ok(envelope) => envelope,
@@ -198,16 +191,31 @@ pub async fn run_queue_workers<DT: Send + Sync + Clone + 'static, ET: std::error
                     continue;
                 }
             };
-            if let Err(e) = job_tx.send(WorkerEvent::Job(job_msg.msg_id, job, envelope)).await {
-                println!("Failed to send job to worker: {}", e);
-            }
-        } else {
-            if config.exit_when_idle && worker_capacity.available_permits() == concurrency {
-                for _ in 0..concurrency {
-                    if let Err(e) = job_tx.send(WorkerEvent::Exit).await {
-                        println!("Failed to send job to worker: {}", e);
-                    }
+
+            tokio::spawn({
+                let pgmq = pgmq.clone();
+                let queue = queue_config.name.clone();
+                let data = data.clone();
+                let result_tx = result_tx.clone();
+                async move {
+                    let data = data.clone();
+                    let result = run_worker(
+                        pgmq,
+                        queue,
+                        WorkerEvent::Job(job_msg.msg_id, job, envelope),
+                        data,
+                    )
+                    .await;
+                    result_tx
+                        .send(result)
+                        .await
+                        .unwrap_or_else(|e| println!("Failed to send result: {}", e));
+                    drop(permit);
                 }
+            });
+        } else {
+            drop(permit);
+            if config.exit_when_idle && worker_capacity.available_permits() == concurrency {
                 return Ok(());
             }
             // println!("No job found, sleeping");
@@ -217,73 +225,55 @@ pub async fn run_queue_workers<DT: Send + Sync + Clone + 'static, ET: std::error
 }
 
 enum WorkerEvent<DT, ET> {
-    Exit,
     Job(i64, Box<dyn Worker<Data = DT, Error = ET>>, JobEnvelope),
 }
 
 async fn run_worker<DT: Send + Sync + Clone + 'static, ET: std::error::Error + Send + Sync>(
-    id: usize,
     pgmq: pgmq::PGMQueue,
     queue: String,
-    job_rx: Arc<Mutex<mpsc::Receiver<WorkerEvent<DT, ET>>>>,
-    result_tx: mpsc::Sender<Result<(), ET>>,
-    worker_capacity: Arc<Semaphore>,
+    job: WorkerEvent<DT, ET>,
     data: WorkerState<DT>,
-) -> Result<(), OxanusError> {
-    tracing::info!("Queue: {} - Worker {} started", queue, id);
+) -> Result<(), ET> {
+    tracing::info!("Queue: {} - Worker started", queue);
+    match job {
+        WorkerEvent::Job(msg_id, job, envelope) => {
+            let result = job.process(&data).await;
+            let is_err = result.is_err();
+            let max_retries = job.max_retries();
+            let retry_delay = job.retry_delay(envelope.meta.retries);
 
-    loop {
-        let worker_capacity = worker_capacity.clone();
-        let job_opt = {
-            let mut locked_rx = job_rx.lock().await;
-            locked_rx.recv().await
-        };
-
-        match job_opt {
-            Some(WorkerEvent::Job(msg_id, job, envelope)) => {
-                let permit = worker_capacity.acquire_owned().await?;
-                let result = job.process(&data).await;
-                let is_err = result.is_err();
-                let max_retries = job.max_retries();
-                let retry_delay = job.retry_delay(envelope.meta.retries);
-                let _ = result_tx.send(result).await;
-
-                let can_archive = if is_err {
-                    if envelope.meta.retries < max_retries {
-                        if let Err(e) = pgmq
-                            .send_delay(&queue, &envelope.with_retries_incremented(), retry_delay)
-                            .await
-                        {
-                            println!("Failed to send job to queue: {}", e);
-                            false
-                        } else {
-                            true
-                        }
+            let can_archive = if is_err {
+                if envelope.meta.retries < max_retries {
+                    if let Err(e) = pgmq
+                        .send_delay(&queue, &envelope.with_retries_incremented(), retry_delay)
+                        .await
+                    {
+                        println!("Failed to send job to queue: {}", e);
+                        false
                     } else {
-                        println!("Job {} failed after {} retries", msg_id, max_retries);
                         true
                     }
                 } else {
+                    println!("Job {} failed after {} retries", msg_id, max_retries);
                     true
-                };
-
-                if can_archive {
-                    if let Err(e) = pgmq.archive(&queue, msg_id).await {
-                        println!("Failed to archive job {}: {}", msg_id, e);
-                    }
                 }
+            } else {
+                true
+            };
 
-                drop(permit);
+            if can_archive {
+                if let Err(e) = pgmq.archive(&queue, msg_id).await {
+                    println!("Failed to archive job {}: {}", msg_id, e);
+                }
             }
-            Some(WorkerEvent::Exit) | None => {
-                println!("Worker {} exiting", id);
-                return Ok(());
-            }
+            return result;
         }
     }
 }
 
-async fn collect_results<ET: std::error::Error + Send + Sync>(mut rx: mpsc::Receiver<Result<(), ET>>) {
+async fn collect_results<ET: std::error::Error + Send + Sync>(
+    mut rx: mpsc::Receiver<Result<(), ET>>,
+) {
     while let Some(result) = rx.recv().await {
         match result {
             Ok(_) => (),
--- src/worker_registry.rs
+++ src/worker_registry.rs
@@ -13,14 +13,20 @@ pub struct WorkerRegistry<DT, ET> {
 
 impl<DT, ET> WorkerRegistry<DT, ET> {
     pub fn new() -> Self {
-        Self { jobs: HashMap::new() }
+        Self {
+            jobs: HashMap::new(),
+        }
     }
 
     pub fn register<T>(&mut self) -> &mut Self
     where
         T: Worker<Data = DT, Error = ET> + serde::de::DeserializeOwned + 'static,
     {
-        fn factory<T: Worker<Data = DT, Error = ET> + serde::de::DeserializeOwned + 'static, DT, ET>(
+        fn factory<
+            T: Worker<Data = DT, Error = ET> + serde::de::DeserializeOwned + 'static,
+            DT,
+            ET,
+        >(
             value: serde_json::Value,
         ) -> Result<BoxedJob<DT, ET>, OxanusError> {
             let job: T = serde_json::from_value(value)?;
@@ -35,11 +41,14 @@ impl<DT, ET> WorkerRegistry<DT, ET> {
         self
     }
 
-    pub fn build(&self, name: &str, json: serde_json::Value) -> Result<BoxedJob<DT, ET>, OxanusError> {
-        let factory = self
-            .jobs
-            .get(name)
-            .ok_or_else(|| OxanusError::GenericError(format!("Job type {} not registered", name)))?;
+    pub fn build(
+        &self,
+        name: &str,
+        json: serde_json::Value,
+    ) -> Result<BoxedJob<DT, ET>, OxanusError> {
+        let factory = self.jobs.get(name).ok_or_else(|| {
+            OxanusError::GenericError(format!("Job type {} not registered", name))
+        })?;
         match factory(json) {
             Ok(job) => Ok(job),
             Err(e) => Err(OxanusError::JobFactoryError(format!(
